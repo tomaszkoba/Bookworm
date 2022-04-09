61e05d0d98e1e1576586d19d3184ed510280c13e
Tomasz Koba <tomaszkoba@outlook.com>
2022-04-09 08:54:42 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Tomasz Koba on 09/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
